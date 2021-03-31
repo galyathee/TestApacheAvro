87f7264bd623e40a4436ddc23b0b0ae1842c1e91
galyathee <galyathee@users.noreply.github.com>
2021-03-31 21:15:08 +0200

    - place first example in dedicated class - write README.md file

---
+++ README.md
@@ -0,0 +1,62 @@
+# TestApacheAvro
+Test of [Apache Avro](https://avro.apache.org/docs/current/gettingstartedjava.html) examples.
+
+## Technologies
+* IDEA
+* Java 8
+* Maven 3.1
+* Apache Avro 1.10.2
+
+## Notes on IDEA
+IDEA will not start the avro-maven-plugin automatically. To workaround this:
+#### Set `mvn compile` to be run before build / rebuild
+Open **View -> Tool windows -> Maven**\
+Right click on *compile* and check *Execute before build* and *Execute before rebuild*.\
+That way, the action "Build module" and "Rebuild module" will always execute the avro maven plugin.
+
+## Project elements
+[1] `pom.xml` contains the avro plugin definition:
+```
+<plugin>
+  <groupId>org.apache.avro</groupId>
+  <artifactId>avro-maven-plugin</artifactId>
+  <version>1.10.2</version>
+  <executions>
+    <execution>
+      <phase>generate-sources</phase>
+      <goals>
+        <goal>schema</goal>
+      </goals>
+      <configuration>
+        <sourceDirectory>${project.basedir}/src/main/avro/</sourceDirectory>
+        <outputDirectory>${project.basedir}/src/main/java/</outputDirectory>
+      </configuration>
+    </execution>
+  </executions>
+</plugin>
+```
+With that declaration `mvn compile` looks for **avsc** files under *${project.basedir}/src/main/avro/* and generates classes under *${project.basedir}/src/main/java/*
+
+[2] `src/main/java/org/avro/example/Main` is the main class
+
+###[1] Example with code generation
+#### Avro *.avsc* file
+`src/main/avro/user.avsc` contains the JSON structure we want to work with as object.\
+
+```
+{"namespace": "pkg.where.to.generate.files",
+ "type": "record",
+ "name": "User",
+ "fields": [
+     {"name": "name", "type": "string"},
+     {"name": "favorite_number",  "type": ["int", "null"]},
+     {"name": "favorite_color", "type": ["string", "null"]}
+ ]
+}
+```
+The generated objects will be placed under the *outputDirectory* specified in the pom.xml, in the Java package given in the *namespace* attribute of the avsc file\
+
+`src/main/java/org/avro/example/codegen/WithCodeGeneration.java` shows a possible use of the generated objects:
+- Instanciation of the generated objects
+- Object serialization
+- Object de-serialization
--- src/main/java/org/avro/example/Main.java
+++ src/main/java/org/avro/example/Main.java
@@ -1,60 +1,12 @@
 package org.avro.example;
 
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.file.SeekableFileInput;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.avro.specific.SpecificDatumWriter;
-import pkg.where.to.generate.files.User;
-
-import java.io.File;
-import java.io.IOException;
+import org.avro.example.codegen.WithCodeGeneration;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Apache Avro example");
-        // basic constructor
-        User user1 = new User();
-        user1.setName("Alyssa");
-        user1.setFavoriteNumber(256);
-
-        // alternate constructor
-        User user2 = new User("Ben", 7, "red");
-
-        // Construct via builder
-        User user3 = User.newBuilder()
-                .setName("Charlie")
-                .setFavoriteColor("blue")
-                .setFavoriteNumber(null)
-                .build();
-
-        // Serialize user1, user2 and user3 to disk
-        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
-        try (DataFileWriter<User> dataFileWriter = new DataFileWriter<>(userDatumWriter)){
-            dataFileWriter.create(user1.getSchema(), new File("users.avro"));
-            dataFileWriter.append(user1);
-            dataFileWriter.append(user2);
-            dataFileWriter.append(user3);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Deserialize Users from disk
-        DatumReader<User> userDatumReader = new SpecificDatumReader<>(User.class);
-        try(DataFileReader<User> dataFileReader = new DataFileReader<>(new SeekableFileInput(new File("users.avro")) , userDatumReader)) {
-            User user = null;
-            while (dataFileReader.hasNext()) {
-            // Reuse user object by passing it to next(). This saves us from
-            // allocating and garbage collecting many objects for files with
-            // many items.
-                user = dataFileReader.next(user);
-                System.out.println(user);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Use of Apache Avro with code generation
+        WithCodeGeneration wcg = new WithCodeGeneration();
+        wcg.showApacheAvroUseWithCodeGeneration();
     }
 }
